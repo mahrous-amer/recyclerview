85a8f1fe0c4b394642b11c2c57d3af00d8958c27
mahrous-amer <eng.mahrous.amer@gmail.com>
2020-12-31 16:51:44 +0800

    Pulling From HTTP Datasource with Volley `Tutorial 4`

--- app/src/main/java/mmu/edu/my/recyclerview/MainActivity.java
+++ app/src/main/java/mmu/edu/my/recyclerview/MainActivity.java
@@ -1,7 +1,9 @@
 package mmu.edu.my.recyclerview;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.LruCache;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -13,25 +15,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.NetworkImageView;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    Version[] versions = {
-            new Version("Cupcake", "API 3", R.drawable.cupcake),
-            new Version("Donut", "API 4", R.drawable.donut),
-            new Version("Eclair", "API 5, 6, 7", R.drawable.eclair),
-            new Version("Froyo", "API 8", R.drawable.froyo),
-            new Version("Gingerbread", "API 9, 10", R.drawable.gingerbread),
-            new Version("Honeycomb", "API 11, 12, 13", R.drawable.honeycomb),
-            new Version("Ice Cream Sandwich", "API 14, 15", R.drawable.ics),
-            new Version("Jelly Bean", "API 16, 17, 18", R.drawable.jellybean),
-            new Version("KitKat", "API 19", R.drawable.kitkat),
-            new Version("Lollipop", "API 21, 22", R.drawable.lollipop),
-            new Version("Marshmallow", "API 23", R.drawable.marshmallow),
-            new Version("Nougat", "API 24, 25", R.drawable.nougat),
-            new Version("Oreo", "API 26, 27", R.drawable.oreo)
-    };
+//    Version[] versions = {
+//            new Version("Cupcake", "API 3", R.drawable.cupcake),
+//            new Version("Donut", "API 4", R.drawable.donut),
+//            new Version("Eclair", "API 5, 6, 7", R.drawable.eclair),
+//            new Version("Froyo", "API 8", R.drawable.froyo),
+//            new Version("Gingerbread", "API 9, 10", R.drawable.gingerbread),
+//            new Version("Honeycomb", "API 11, 12, 13", R.drawable.honeycomb),
+//            new Version("Ice Cream Sandwich", "API 14, 15", R.drawable.ics),
+//            new Version("Jelly Bean", "API 16, 17, 18", R.drawable.jellybean),
+//            new Version("KitKat", "API 19", R.drawable.kitkat),
+//            new Version("Lollipop", "API 21, 22", R.drawable.lollipop),
+//            new Version("Marshmallow", "API 23", R.drawable.marshmallow),
+//            new Version("Nougat", "API 24, 25", R.drawable.nougat),
+//            new Version("Oreo", "API 26, 27", R.drawable.oreo)
+//    };
+    ArrayList<Version> versions = new ArrayList<Version>();
+    RequestQueue queue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,8 +57,37 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.recycleView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         MyAdapter myAdapter = new MyAdapter();
-        myAdapter.addElements(versions);
+//        myAdapter.addElements(versions);
         recyclerView.setAdapter(myAdapter);
+        queue = Volley.newRequestQueue(this);
+        JsonArrayRequest request = new JsonArrayRequest( Request.Method.GET, " https://raw.githubusercontent.com/kenobicjj/android/main/tutorial4.json ",
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        versions.clear();
+                        for(int i = 0;i < response.length();i++) {
+                            try {
+                                JSONObject item = response.getJSONObject(i);
+                                String name = item.getString("name");
+                                String description = item.getString("description");
+                                String icon = item.getString("icon");
+                                Version version =new Version(name, description, icon);
+                                versions.add(version);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        myAdapter.addElements(versions);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+        queue.add(request);
     }
 
     class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
@@ -57,10 +103,23 @@ public class MainActivity extends AppCompatActivity {
         public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position) {
             holder.textView.setText(elements.get(position).getName());
             holder.textView2.setText(elements.get(position).getDescription());
-            holder.imageView.setImageResource(elements.get(position).getIcon());
-            holder.textView.setText(elements.get(position).getName());
-            holder.textView2.setText(elements.get(position).getDescription());
-            holder.imageView.setImageResource(elements.get(position).getIcon());
+//            holder.imageView.setImageResource(elements.get(position).getIcon());
+//            holder.textView.setText(elements.get(position).getName());
+//            holder.textView2.setText(elements.get(position).getDescription());
+//            holder.imageView.setImageResource(elements.get(position).getIcon());
+            String iconUrl = "https://raw.githubusercontent.com/kenobicjj/android/main/"+elements.get(position).getIcon();
+            final LruCache<String, Bitmap> cache =new LruCache<String, Bitmap>(20);
+            holder.imageView.setImageUrl(iconUrl, new ImageLoader(queue, new
+                    ImageLoader.ImageCache() {
+                        @Override
+                        public Bitmap getBitmap(String url) {
+                            return cache.get(url);
+                        }
+                        @Override
+                        public void putBitmap(String url, Bitmap bitmap) {
+                            cache.put(url, bitmap);
+                        }
+                    }));
         }
 
         @Override
@@ -68,16 +127,16 @@ public class MainActivity extends AppCompatActivity {
             return elements.size();
         }
 
-        public void addElements(Version[] versions) {
+        public void addElements(ArrayList<Version> versions) {
             elements.clear();
-            elements.addAll(Arrays.asList(versions));
+            elements.addAll(versions);
             notifyDataSetChanged();
         }
 
         class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
             public TextView textView;
             public TextView textView2;
-            public ImageView imageView;
+            public NetworkImageView imageView;
 
             public MyViewHolder(@NonNull View itemView){
                 super(itemView);
--- app/src/main/java/mmu/edu/my/recyclerview/Version.java
+++ app/src/main/java/mmu/edu/my/recyclerview/Version.java
@@ -4,9 +4,9 @@ public class Version {
     // including 3 member variables
     private String name;
     private String description;
-    private int icon;
+    private String icon;
 
-    public Version(String name, String description, int icon) {
+    public Version(String name, String description, String icon) {
         this.name = name;
         this.description = description;
         this.icon = icon;
@@ -20,7 +20,7 @@ public class Version {
         return description;
     }
 
-    public int getIcon() {
+    public String getIcon() {
         return icon;
     }
 }
